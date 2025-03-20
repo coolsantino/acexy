610de596275d94f65c8c9c1bdd3dc8fce829d340
coolsantino <920767+coolsantino@users.noreply.github.com>
2025-03-20 20:30:20 +0300

    Update aceid.go

--- acexy/lib/acexy/aceid.go
+++ acexy/lib/acexy/aceid.go
@@ -8,7 +8,7 @@ import (
 )
 
 type AceID struct {
-	id       string
+	content_id string
 	infohash string
 }
 
@@ -16,14 +16,14 @@ type AceID struct {
 type AceIDType string
 
 // Create a new `AceID` object
-func NewAceID(id, infohash string) (AceID, error) {
-	if id == "" && infohash == "" {
-		return AceID{}, errors.New("one of `id` or `infohash` must have a value")
+func NewAceID(content_id, infohash string) (AceID, error) {
+	if content_id == "" && infohash == "" {
+		return AceID{}, errors.New("one of `content_id` or `infohash` must have a value")
 	}
-	if id != "" && infohash != "" {
-		return AceID{}, errors.New("only one of `id` or `infohash` can have a value")
+	if content_id != "" && infohash != "" {
+		return AceID{}, errors.New("only one of `content_id` or `infohash` can have a value")
 	}
-	return AceID{id: id, infohash: infohash}, nil
+	return AceID{content_id: content_id, infohash: infohash}, nil
 }
 
 // Get the valid AceStream ID. If the `infohash` is set, it will be returned,
@@ -32,11 +32,11 @@ func (a AceID) ID() (AceIDType, string) {
 	if a.infohash != "" {
 		return "infohash", a.infohash
 	}
-	return "id", a.id
+	return "content_id", a.content_id
 }
 
 // Get the AceStream ID as a string
 func (a AceID) String() string {
-	idType, id := a.ID()
-	return fmt.Sprintf("{%s: %s}", idType, id)
+	idType, content_id := a.ID()
+	return fmt.Sprintf("{%s: %s}", idType, content_id)
 }
